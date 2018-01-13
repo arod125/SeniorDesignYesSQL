9bc35cb24758d5e94ce78ef5be9ea983adb41974
arod125 <asr322@drexel.edu>
2018-01-12 22:10:59 -0500

    Making use of bootstrap for Main Interface and workspace
    Using bootstrap to make up the pages a lot more for the front end.

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,12 +1,12 @@
 let navOpen = false;
 function profileNav() {
 	if(navOpen){
-		document.getElementById("profileNav").style.width = "0px";
-		document.getElementById("content").style.marginLeft = "0";
+		document.getElementById("profileNav").style.width = "0%";
+		document.getElementById("content").style.marginLeft = "0%";
 		navOpen = false;
 	} else {
-		document.getElementById("profileNav").style.width = "500px";
-		document.getElementById("content").style.marginLeft = "500px";
+		document.getElementById("profileNav").style.width = "80%";
+		document.getElementById("content").style.marginLeft = "80%";
 		navOpen = true;
 	}
 }
